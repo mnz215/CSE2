410d578f57690be0cd934ca274b6dd8d3a3d5173
mnz215 <mnz215@lehigh.edu>
2015-01-27 22:50:02 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,19 @@
+//Murphy Zingsheim
+//Homework #1 WelcomeClass
+//Created 1-27-2015
+//Instructor: Brian Chen
+
+public class WelcomeClass{
+    public static void main(String[] args){
+        System.out.println("   -----------");
+        System.out.println("   | WELCOME |");
+        System.out.println("   -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^ ");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("<-M--N--Z--2--1--5->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v  v  v  v  v  v  ");
+        System.out.println("I enjoy spending as much time outside as possible. This past summer");
+        System.out.println("my friends and I went on a 5 day canoeing trip through the Boundary Waters in Minnesota.");
+    }
+}
---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,16 @@
+//Murphy Zingsheim
+//Program 1
+//Hello World Java Program
+
+public class HelloWorld{
+    public static void main(String[] args) {
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("#                      #");
+        System.out.println("#  Hello World 2015!!  #");
+        System.out.println("#                      #");
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("test");
+    }
+}
